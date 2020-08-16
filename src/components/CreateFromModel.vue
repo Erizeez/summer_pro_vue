f39c3b02822a858179a2f479ad48b9368d9c841b
Erizeez <820540373@qq.com>
2020-08-16 22:14:23 +0800

    Update CreateFromModel.vue

--- src/components/CreateFromModel.vue
+++ src/components/CreateFromModel.vue
@@ -3,9 +3,55 @@
         <el-card class="box-card">
             <el-tabs :tab-position="tabPosition">
 
-                <el-tab-pane label="系统模板">
-                    <el-table :data="systemModels" stripe style="width: 950px; left: 10%">
+                <el-tab-pane>
+                    <span slot="label" class="paneTitle">系统模板</span>
+                    <el-row :gutter="20">
+                        <el-col :offset="3" :span="2">
+                            <div>
+                                <span class="tableTitle">序号</span>
+                            </div>
+                        </el-col>
+                        <el-col :span="14">
+                            <div class="talk-box">
+                                <span class="tableTitle">模板名</span>
+                            </div>
+                        </el-col>
+                        <el-col :span="2">
+                            <div>
+                                <span class="tableTitle">操作</span>
+                            </div>
+                        </el-col>
+                    </el-row>
 
+                    <el-col :span="24" style="width: 95%; margin: 0px 0px 20px 0px;">
+                        <div style="overflow: hidden;" v-for="(item,i) in systemModels" :key="i" class="author-title">
+                            <el-row :gutter="20">
+                                <el-col :offset="3" :span="2">
+                                    <div>
+                                        <span class="tablePart">{{i+1}}</span>
+                                    </div>
+                                </el-col>
+                                <el-col :span="13">
+                                    <div>
+                                        <span class="tablePart">{{item.name}}</span>
+                                    </div>
+                                </el-col>
+                                <el-col :span="6">
+                                    <div>
+                                        <el-button type="info" style="margin:0 auto" @click="showSystemModel(scope.row)" icon="el-icon-view">预览
+                                        </el-button>
+                                        <el-button type="primary" style="margin:0 auto" @click="useModel(scope.row)">使用模板创建文档
+                                        </el-button>
+                                    </div>
+                                </el-col>
+                            </el-row>
+                            <el-divider></el-divider>
+                        </div>
+                    </el-col>
+
+                    <!-- <el-table :data="systemModels" stripe style="width: 950px; left: 10%">
+                        
+                        <el-divider></el-divider>
                         <el-table-column type="index" label="序号"></el-table-column>
                         <el-table-column prop="name" label="模板名" width="600" style="text-align: center">
                         </el-table-column>
@@ -20,10 +66,11 @@
                             </template>
                         </el-table-column>
 
-                    </el-table>
+                    </el-table> -->
                 </el-tab-pane>
 
-                <el-tab-pane label="我的模板" style="height: 100px;">
+                <el-tab-pane style="height: 100px;">
+                    <span slot="label" class="paneTitle">我的模板</span>
                     <div v-if="MyModels.length==0" style="text-align: center;position: relative; top: 50%;">
                         <span style="font-size: 30px; color: #888;">还没有自定义模板，快将觉得满意的文件添加为模板吧！</span>
                     </div>
@@ -163,5 +210,20 @@
     left: 50%;
     top: 100px;
     transform :translate(-50%, 0%);
+    }
+    .tableTitle{
+        font-size: 20px;
+        font-weight: bold;
+        color: #888;
+    }
+    .tablePart{
+        font-size: 20px;
+        //font-weight: bold;
+        color: #666;
+    }
+    .paneTitle{
+        font-size: 20px;
+        ///font-weight: bold;
+        color: #888;
     }
 </style>
