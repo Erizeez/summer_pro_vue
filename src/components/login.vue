b860cf7312f28ed2421e5dac71a9a290e4cd278f
shanyuaa <3039067675@qq.com>
2020-08-16 14:21:06 +0800

    修改密码功能；登录完善

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="login_container">
     <div class="login_box">
+
       <div class="avatar_box">
-        <img src="../assets/rlc-logo.png" alt="">
+        <img src="../assets/logo.png" style="width: 100px; height: 100px; border-radius: 50%; background-color: #eeeeee">
       </div>
-      <div class="big_stress">
-        欢迎使用金刚石文档
-      </div>
+
       <el-form :model="loginForm" :rules="loginformRules" class="login_form" ref="loginformRef">
         <el-form-item prop="name">
           <el-input v-model="loginForm.name" placeholder="请输入用户名">
@@ -19,7 +18,7 @@
           </el-input>
         </el-form-item>
         <el-form-item class = "btns">
-          <el-button type="info" round @click="goRegister">前往注册</el-button>
+          <el-button type="primary" round @click="goRegister">注册</el-button>
           <el-button type="success" round @click="login">登录</el-button>
         </el-form-item>
       </el-form>
@@ -42,7 +41,6 @@
           ],
           password:[
             { required: true, message: '请输入密码', trigger: 'blur' },
-            { min: 3, max: 15, message: '长度在 3 到 15 个字符', trigger: 'blur' }
           ]
         }
       }
@@ -76,62 +74,42 @@
   }
 </script>
 
-<style lang="less" scoped>
+<style scoped>
 .login_container{
   height: 100%;
   background-image: url("../assets/login/login_bg.jpg");
-  }
+}
   .login_box{
-    width: 450px;
-    height: 380px;
-    background-color: rgba(255, 255, 255, 0.5);
-    border-radius: 30px;
+    height: 450px;
+    width: 400px;
+    background-color: aliceblue;
+    border-radius: 5px;
     position: absolute;
     left: 50%;
-    top: 50%;
-    transform :translate(-50%, -50%);
-    box-shadow: 0px 5px 35px 20px rgba(0, 0, 0, 0.3);
-    .avatar_box{
-      height: 130px;
-      width: 130px;
-      border: 1px solid #eee;
-      border-radius: 50%;
-      padding: 10px;
-      box-shadow: 0 0 10px #ddd;
-      position: absolute;
-      left: 50%;
-      transform :translate(-50%, -50%);
-      background-color: #fff;
-      box-shadow: 0px 2px 15px 3px rgba(0, 0, 0, 0.3);
-      img{
-        width: 100%;
-        height: 100%;
-        border-radius: 50%;
-        background-color: #eee;
-      }
-    }
+    top:50%;
+    transform: translate(-50%, -50%);
   }
-  .big_stress{
-    font-family: "Microsoft YaHei";
-    font-size: 30px;
-    color: #aaa;
+  .avatar_box{
+    height: 100px;
+    width: 100px;
+    border: 2px solid #eeeeee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #dddddd;
     position: absolute;
-    text-align: center;
-    top: 100px;
-    width: 100%;
-    margin: 0 auto;
-    padding: 0 20px;
-    box-sizing: border-box;
-  }
-  .login_form{
-    position: absolute;
-    bottom: 0;
-    width: 100%;
-    padding: 0 20px;
-    box-sizing: border-box;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #ffffff;
   }
   .btns{
     display: flex;
-    justify-content: flex-end;
+    justify-content: center;
+  }
+  .login_form{
+    position: absolute;
+    top: 100px;
+    width: 100%;
+    padding: 20px;
+    box-sizing: border-box;
   }
 </style>
